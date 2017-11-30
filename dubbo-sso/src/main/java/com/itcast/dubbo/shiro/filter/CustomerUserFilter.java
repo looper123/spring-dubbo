8a80142bef6892c96d730244aee10e37fc210be4
looper <m17701848923@163.com>
2017-11-30 12:47:49 +0800

    module delete

--- dubbo-sso/src/main/java/com/itcast/dubbo/shiro/filter/CustomerUserFilter.java
+++
@@ -1,9 +0,0 @@
-package com.itcast.dubbo.shiro.filter;
-
-import org.apache.shiro.web.filter.authc.UserFilter;
-
-/**
- * Created by zebon lu on 2017/7/12.
- */
-public class CustomerUserFilter extends UserFilter {
-}
